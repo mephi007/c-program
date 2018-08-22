12ba07f926a8e8e2e68626c919f1ab9f10ef9c41
mephi007 <roysumit949@gmail.com>
2018-08-22 18:40:10 +0530

    inverse_star_pattern

---
+++ inverse_star.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	int n;
+		cout<<"Enter the number\n";
+	cin>>n;
+	for(int i = 1 ; i <=5 ; i++)
+	{
+		for(int j = 5; j>=i ; j--)
+		{
+
+			cout<<"*";		}
+			cout<<endl;
+	}
+	return 0;
+}

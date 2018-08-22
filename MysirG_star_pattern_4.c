65fbdbc59e3c8565977769e4aed5540537bcdefd
mephi007 <roysumit949@gmail.com>
2018-08-22 22:46:42 +0530

    star triangle

---
+++ MysirG_star_pattern_4.c
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"enter the number:\n";
+	cin>>n;
+	for(int i=1 ; i<=n; i++)
+	{
+		for(int j = 1 ; j < 2*n ; j++)
+		{
+			if((n-i)< j && j <(n+i))
+			{
+				cout<<"*";
+
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+	cout<<endl;
+	}
+
+	return 0;
+
+
+}

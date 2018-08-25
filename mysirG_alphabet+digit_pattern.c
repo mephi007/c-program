5bfa3c481c7883e628664f64f5b2a1fe5dc68a9c
mephi007 <roysumit949@gmail.com>
2018-08-25 08:44:43 +0530

    alphabet + digit

---
+++ mysirG_alphabet+digit_pattern.c
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+int i,j,n,num;
+char k;
+
+cout<<"enter row\n";
+cin>>n;
+
+for(i = 1 ; i<= n ; i++)
+{
+	k= 'A'; num=1;
+	for(j=1 ; j<= 2*n ; j++)
+	{
+		if(j>=n+1-i && j<= n+i)
+		{
+			if(j>= n+1-i && j<= n)
+			{
+				cout<<k;
+				k++;
+			}
+			else
+			{
+				cout<<num;
+				num++;
+			}
+
+		}
+		else
+		{
+			cout<<" "; 
+		}
+	}
+	cout<<endl;
+}
+}
